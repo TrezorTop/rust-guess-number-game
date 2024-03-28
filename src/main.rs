f03f4a3217c73fdbc757b104cb7666f1b51aafac
AndreySeluykov <trezortop92@gmail.com>
2024-03-29 01:06:57 +0500

    refactor: add struct SecretNumber

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,28 @@ use std::io;
 
 use rand::Rng;
 
+pub struct SecretNumber {
+    value: i32,
+}
+
+impl SecretNumber {
+    pub fn new(value: i32) -> SecretNumber {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {value}.",);
+        }
+
+        SecretNumber { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number: SecretNumber = SecretNumber::new(rand::thread_rng().gen_range(1..=100));
 
     loop {
         println!("Please input your guess.");
@@ -17,9 +35,9 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = match guess.trim().parse() {
-            Result::Ok(num) => num,
-            Result::Err(_) => {
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
                 println!("Please enter a number");
                 continue;
             }
@@ -27,7 +45,7 @@ fn main() {
 
         println!("You guessed: {guess}");
 
-        match guess.cmp(&secret_number) {
+        match guess.cmp(&secret_number.value) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
